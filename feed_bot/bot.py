1c6d9bcbcbac4ade4af9524c2b5e877f55fa16c0
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-01-27 13:10:43 -0500

    refactor bot to being getting channel_id from context of command and pass subreddit

--- feed_bot/bot.py
+++ feed_bot/bot.py
@@ -24,31 +24,36 @@ class FeedBot(commands.Bot):
         print("------")
 
     @tasks.loop(seconds=60)
-    async def post_subreddit(self):
-        """Returns New Posts In Affiliate Reddit"""
-        channel = self.get_channel(1198396321409802372)
-        affiliate_marketing = Reddit("Affiliatemarketing")
+    async def post_subreddit(self, *args, **kwargs):
+        """Returns New Posts for a subreddit"""
+        cmd_ctx = kwargs.get("cmd_ctx")
+        channel_id = cmd_ctx.message.channel.id
+        subreddit = kwargs.get("subreddit")
+        channel = self.get_channel(channel_id)
+        affiliate_marketing = Reddit(subreddit)
         embeds = await affiliate_marketing.get_embedded_posts(channel)
         if embeds:
             for embed in embeds:
                 await channel.send(embed=embed)
 
-    # @post_subreddit.before_loop
-    # async def before_my_task(self):
-    #     await self.wait_until_ready()  # wait until the bot logs in
+    @post_subreddit.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()  # wait until the bot logs in
 
 class RedditCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command(name="rss_subreddit")
-    async def rss_subreddit(self, ctx):
+    async def rss_subreddit(self, ctx, arg, *args, **kwargs):
         """Get updates when a new posts has been added to a subreddit
         
-        Command: $rss_subreddit <subreddit_name>
+        Add subreddit feed: $rss_subreddit <subreddit_name>
         """
-        self.bot.post_subreddit.start()
-        await ctx.send("starting...")
+        kwargs["cmd_ctx"] = ctx
+        kwargs["subreddit"] = arg
+        self.bot.post_subreddit.start(*args, **kwargs)
+        await ctx.send(f"**Starting For r/{arg}...**")
 
     @commands.command(name="rm_subreddit")
     async def rm_subreddit(self, ctx):
