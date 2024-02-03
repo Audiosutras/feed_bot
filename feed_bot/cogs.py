4fa70f06905a52ff7a0b7ad5bd12654107d7d0d3
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-02-03 10:08:45 -0500

    refactor

--- feed_bot/cogs.py
+++ feed_bot/cogs.py
@@ -15,7 +15,7 @@ class RedditRSS(commands.Cog):
         self.bot = bot
 
     @commands.group(name="subreddit")
-    async def subreddit(self, ctx):
+    async def subreddit(self, ctx: commands.Context) -> None:
         """Group command for managing channel subreddit rss feeds"""
         if ctx.invoked_subcommand is None:
             await ctx.send(
@@ -23,42 +23,53 @@ class RedditRSS(commands.Cog):
             )
 
     @subreddit.command(name="add")
-    async def add(self, ctx, arg):
+    async def add(self, ctx: commands.Context, arg: str) -> None:
         """Add subreddit(s) as an rss feed for this channel.
 
         If a user is trying to add a subreddit that is already in the db for a channel the
         existing document is returned. If a subreddit for a channel is not already in the
         db the subreddit is created in the db.
+
+        Args:
+            ctx (commands.Context): Invocation Context
+            arg (str): the subreddit or comma separated list of subreddits to add.
         """
         channel = ctx.message.channel
         channel_id = channel.id
-        subreddit = arg
-        if arg.includes(","):
-            subreddit = arg.split(",")
-        doc_dict = {"channel_id": channel_id, "subreddit": subreddit}
-        filter_dict = {
-            **doc_dict,
-            "title": {"$exists": False},
-            "description": {"$exists": False},
-            "link": {"$exists": False},
-            "sent": {"$exists": False},
-        }
-        doc = await self.bot.reddit_collection.find_one(filter_dict)
-        if doc:
-            await channel.send(f"**Already subscribed to r/{subreddit}**")
+        subreddit_arg = arg
+        if "," in arg:
+            subreddit_arg = arg.split(",")
         else:
-            await self.bot.reddit_collection.insert_one(doc_dict)
-            await channel.send(f"**Subscribed to r/{subreddit} 'new' listings**")
+            subreddit_arg = [arg]
+
+        for sa in subreddit_arg:
+            subreddit = sa
+            if sa.startswith("r/"):  # r/<subreddit> or <subreddit> is acceptable
+                subreddit = sa[2:]
+            doc_dict = {"channel_id": channel_id, "subreddit": subreddit}
+            filter_dict = {
+                **doc_dict,
+                "title": {"$exists": False},
+                "description": {"$exists": False},
+                "link": {"$exists": False},
+                "sent": {"$exists": False},
+            }
+            doc = await self.bot.reddit_collection.find_one(filter_dict)
+            if doc:
+                await channel.send(f"**Already subscribed to r/{subreddit}**")
+            else:
+                await self.bot.reddit_collection.insert_one(doc_dict)
+                await channel.send(f"**Subscribed to r/{subreddit} 'new' listings**")
 
     @subreddit.command(name="start")
-    async def start(self, ctx):
+    async def start(self, ctx: commands.Context) -> None:
         """Stops rss feeds feeds service"""
         self.bot.post_subreddit.start(ctx)
         self.bot.pull_subreddit.start(ctx)
         await ctx.send("**Starting reddit rss feed service...**")
 
     @subreddit.command(name="stop")
-    async def stop(self, ctx):
+    async def stop(self, ctx: commands.Context) -> None:
         """Stops rss feeds feeds service"""
         self.bot.post_subreddit.stop()
         self.bot.pull_subreddit.stop()
